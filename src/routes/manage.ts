4721321762d601839480478fd1f226944b697b5a
Ultra-Code <mega.alpha100@gmail.com>
2020-05-22 22:40:42 +0000

    Add route directory and a route for koa

---
+++ src/routes/manage.ts
@@ -0,0 +1,15 @@
+import Router from "koa-router";
+
+const router = new Router();
+router.get("/koa", async (ctx) => {
+  try {
+    ctx.body = {
+      success: "Koa Success",
+      data: "Koa Route is Awesome",
+    };
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+export default router;
