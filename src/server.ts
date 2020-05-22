f57f740991f446c685ec364f56aff0414005f8b3
Ultra-Code <mega.alpha100@gmail.com>
2020-05-22 22:42:01 +0000

    Refactor server.ts

--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,21 @@ import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import cors from "koa2-cors";
 import logger from "koa-logger";
+import manage from "./routes/manage";
+import { config } from "./config";
 
 const app = new Koa();
 
-const PORT = 9000;
+const PORT = config.port;
 
 app.use(bodyParser());
 app.use(cors({ origin: "*" }));
 app.use(logger());
 
+app.use(manage.routes());
+
 const server = app
-  .listen(PORT, "Malpha", async () => {
+  .listen(PORT, async () => {
     console.log("Server is listening on port : ", PORT);
   })
   .on("error", (error) => console.error(error));
