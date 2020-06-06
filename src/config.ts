ffef5bfbdc5ebd4538fca73bfa428686acd97dcf
Ultra-Code <mega.alpha100@gmail.com>
2020-06-06 16:45:27 +0000

    Remove unneeded funtions

--- src/config.ts
+++ src/config.ts
@@ -2,18 +2,5 @@ interface portConfig {
   port: Number;
 }
 export const config: portConfig = {
-  port: Number(process.env.PORT) || 9000,
+  port: Number(process.env.PORT) || 3000,
 };
-
-type overload = {
-  (ins: number, out?: number): string;
-};
-
-const myOverload: overload = (ins, out) => {
-  let hold = String(ins);
-  if (typeof out === "number") {
-    hold += String(out);
-  }
-  return hold;
-};
-myOverload(3, 3);
