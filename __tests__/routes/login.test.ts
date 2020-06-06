761f98831999cd5c3a9192b76007e403e3bc9d9f
Ultra-Code <mega.alpha100@gmail.com>
2020-06-06 16:47:00 +0000

    Add test for login routes

---
+++ __tests__/routes/login.test.ts
@@ -0,0 +1,16 @@
+import server from "../../src/server"
+import request from "supertest"
+
+afterEach(
+    (done)=>{server.close();
+          done();})
+
+describe("test route/login",()=>{
+    it("logs a user into our system",async ()=>{
+        const responds = await request(server).post("/login").send({response:"you have logged in"});
+
+        expect(responds.status).toBe(201);
+        expect(responds.body).toEqual({response:"you are logged in"});
+        expect(responds.type).toEqual("application/json")
+    })
+})
