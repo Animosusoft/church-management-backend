fbf05e2b158794bcc756dfda1431cd37cc8636d4
Ultra-Code <mega.alpha100@gmail.com>
2020-06-20 02:59:46 +0000

    Route for performing some basic action on members

---
+++ src/routes/member.ts
@@ -0,0 +1,43 @@
+import Router from "koa-router"
+import {ChurchMember} from "../models/index"
+
+const router = new Router();
+
+router.post("/member",async (ctx)=>{
+       const church_member_info:any= ctx.request.body
+       const member = new ChurchMember(church_member_info);
+     member.save((errorInDbTransaction:Error)=>{
+        if(errorInDbTransaction){
+            console.error("There Is an error \n")
+            console.error(errorInDbTransaction);
+            ctx.status =401;
+            }
+            ctx.body={
+                errors:{
+                    message:"Some Validations In Schema failed"
+            }
+        }})
+     ctx.status = 201;
+     ctx.body={
+         message:"A church Member Has been created successfully"
+        }
+})
+
+router.get("/member",async (ctx)=>{
+    try {
+       await ChurchMember.find((error:Error,members:Document)=>{
+           if(error){
+               console.error("The Following Error Occured While Try To Get Data: ",error)
+           }else{
+               console.log("We Can Get Our Data\n")
+               ctx.body={members};
+           }
+       })
+    } catch (error) {
+        console.error("")
+        throw error;
+    }
+
+})
+
+export default router;
