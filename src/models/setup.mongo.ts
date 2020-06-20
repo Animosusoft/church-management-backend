3eaf0e47ad88dc08dfc7b1a40f593ac7cf198177
Ultra-Code <mega.alpha100@gmail.com>
2020-06-20 06:17:03 +0000

    Add UseCreateIndex option to true to fix a deprecation warning after adding a unique identifier

--- src/models/setup.mongo.ts
+++ src/models/setup.mongo.ts
@@ -1,11 +1,14 @@
-import mongoose from "mongoose"
-import {mongo_uri} from "../config/index"
+import mongoose from "mongoose";
+import { mongo_uri } from "../config/index";
 
-mongoose.connect(mongo_uri,{useUnifiedTopology:true,useNewUrlParser: true})
-
-const db= mongoose.connection;
-db.on("error",console.error.bind("connection error"))
-db.once("open",()=>{
-    console.log("Connection To The Database has been established")
+mongoose.connect(mongo_uri, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
 });
 
+const db = mongoose.connection;
+db.on("error", console.error.bind("connection error"));
+db.once("open", () => {
+    console.log("Connection To The Database has been established");
+});
