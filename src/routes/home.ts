395a9e8e8d7b9cedfd5865c50dc8b04e6eb08b95
Ultra-Code <mega.alpha100@gmail.com>
2020-06-25 15:13:53 +0000

    Add api prefix

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,14 +1,15 @@
 import Router from "koa-router";
 
-const router = new Router();
-router.get("/koa", async (ctx) => {
+const router = new Router({ prefix: "/api/v1" });
+router.get("/", async (ctx) => {
     try {
         ctx.body = {
             success: "Koa Success",
             data: "Koa Route is Awesome",
+            message: "I am Still Working On Developing the homepage",
         };
     } catch (error) {
-        console.error(error);
+        ctx.throw(error);
     }
 });
 
