e8e222543fd2970c7883cc5a4021bb0128269b60
Ultra-Code <mega.alpha100@gmail.com>
2020-06-25 03:36:54 +0000

    Change Promise based Program structure to Async

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -5,34 +5,32 @@ const router = new Router();
 
 router.post("/member", async (ctx) => {
     const church_member_info: any = ctx.request.body;
+    console.log(church_member_info);
     const member = new ChurchMember(church_member_info);
-    await member
-        .save()
-        .then(() => {
-            ctx.status = 201;
-            ctx.body = {
-                message: "A church Member Has been created successfully",
-            };
-        })
-        .catch((reason: Error) => {
-            console.error(reason);
-            ctx.status = 400;
-            ctx.body = {
-                message: " Some Validations of the Schema Failed",
-                error: reason.message,
-            };
-        });
+    try {
+        await member.save();
+        ctx.status = 201;
+        ctx.body = {
+            message: "A church Member Has been created successfully",
+        };
+    } catch (error) {
+        console.error(error);
+        ctx.status = 400;
+        ctx.body = {
+            message: " Some Validations of the Schema Failed",
+            error: error.message,
+        };
+    }
 });
 
 router.get("/member", async (ctx) => {
-    await ChurchMember.find()
-        .then((members) => {
-            console.log("Data is ready to be retrived ");
-            ctx.body = { members };
-        })
-        .catch((reason: Error) => {
-            console.error(reason);
-        });
+    try {
+        const members = await ChurchMember.find();
+        console.log("Data is ready to be retrived ");
+        ctx.body = { members };
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 export default router;
