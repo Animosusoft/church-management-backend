57677a8762ffdef9d6e34607df96401e976914cf
Ultra-Code <mega.alpha100@gmail.com>
2020-06-25 14:50:14 +0000

    Add route to retrieve member by membership_id

--- src/routes/member.ts
+++ src/routes/member.ts
@@ -5,7 +5,6 @@ const router = new Router();
 
 router.post("/member", async (ctx) => {
     const church_member_info: any = ctx.request.body;
-    console.log(church_member_info);
     const member = new ChurchMember(church_member_info);
     try {
         await member.save();
@@ -14,22 +13,32 @@ router.post("/member", async (ctx) => {
             message: "A church Member Has been created successfully",
         };
     } catch (error) {
-        console.error(error);
         ctx.status = 400;
         ctx.body = {
             message: " Some Validations of the Schema Failed",
             error: error.message,
         };
+        ctx.throw(error);
     }
 });
 
 router.get("/member", async (ctx) => {
     try {
-        const members = await ChurchMember.find();
+        const All_Church_Members = await ChurchMember.find();
         console.log("Data is ready to be retrived ");
-        ctx.body = { members };
+        ctx.body = { All_Church_Members };
     } catch (error) {
-        console.error(error);
+        ctx.throw(error);
+    }
+});
+
+router.get("/member:membership_id", async (ctx) => {
+    try {
+        const members_id = ctx.request.query;
+        const member = await ChurchMember.findOne({ membership_id: members_id });
+        ctx.response.body = { member };
+    } catch (error) {
+        ctx.throw(error);
     }
 });
 
