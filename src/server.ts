f67598926473399e1117e10e4a6021650fc270b7
Ultra-Code <mega.alpha100@gmail.com>
2020-06-26 08:47:20 +0000

    Add allowedMethods middleware to all routes

--- src/server.ts
+++ src/server.ts
@@ -2,27 +2,27 @@ import Koa from "koa";
 import bodyParser from "koa-bodyparser";
 import cors from "koa2-cors";
 import logger from "koa-logger";
-import * as Routes from "./routes/index"
+import * as Routes from "./routes/index";
 import { environment } from "./config/index";
-import compress from "koa-compress"
+import compress from "koa-compress";
 
 const app = new Koa();
 
 const PORT = environment.port;
 
-app.use(bodyParser());
-app.use(cors({ origin: "*" }));
-app.use(logger());
-app.use(compress())
+app.use(bodyParser())
+    .use(cors({ origin: "*" }))
+    .use(logger())
+    .use(compress());
 
-app.use(Routes.Home.routes());
-app.use(Routes.Login.routes())
-app.use(Routes.Member.routes())
+app.use(Routes.Home.routes()).use(Routes.Home.allowedMethods());
+app.use(Routes.Login.routes()).use(Routes.Login.allowedMethods());
+app.use(Routes.Member.routes()).use(Routes.Member.allowedMethods());
 
 const server = app
-  .listen(PORT, async () => {
-    console.log("Server is listening on port : ", PORT);
-  })
-  .on("error", (error) => console.error(error));
+    .listen(PORT, async () => {
+        console.log("Server is listening on port : ", PORT);
+    })
+    .on("error", (error) => console.error(error));
 
 export default server;
